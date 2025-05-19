f1b98c97470cae52f52cb640925ef3a69ece1b1d
Alexander Konovalov <alexander.konovalov.work@gmail.com>
2025-05-19 20:48:10 +0200

    added popup form

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fresh fruits and vegetables online store
+
+## Project Setup
+
+### Requirements
+
+- [Node.js](https://nodejs.org/) (v14+ recommended)
+- [npm](https://www.npmjs.com/) (comes with Node.js)
+
+## Scripts
+
+### Development mode
+```
+npm install
+npm run start:dev
+```
+
+### Production mode
+```
+npm install
+npm run build
+npm start 
+```
--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,27 @@ function setActiveSlide(idx) {
         sliderElementsBar.style.transform = `translateX(${-200 * idx}px)`
     }
 }
+
+function handleClosePopup() {
+    const popupContainerElement = document.querySelector('.popup_container');
+    popupContainerElement.style.display = 'none';
+}
+
+function handleAddToCart() {
+    const popupContainerElement = document.querySelector('.popup_container');
+    popupContainerElement.style.display = 'flex';
+}
+
+window.onload = () => {
+    const addToCartButtonElements = document.getElementsByClassName('add_to_cart_btn');
+    for (let i=0; i < addToCartButtonElements.length; i++) {
+        addToCartButtonElements[i].addEventListener('click', handleAddToCart);
+    }
+}
+
+window.onbeforeunload = () => {
+    const addToCartButtonElements = document.getElementsByClassName('add_to_cart_btn');
+    for (let i=0; i < addToCartButtonElements.length; i++) {
+        addToCartButtonElements[i].removeEventListener('click', handleAddToCart);
+    }
+}
